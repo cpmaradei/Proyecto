7ea4f0fb7997216a69312e33b822f0bd1f97f4c0
cpmaradei <cpmaradey@gmail.com>
2025-06-02 21:06:51 -0500

    xml solicitar

---
+++ app/src/main/java/com/example/proyecto/solicitarFragment.java
@@ -0,0 +1,49 @@
+package com.example.proyecto;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class solicitarFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public solicitarFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static solicitarFragment newInstance(String param1, String param2) {
+        solicitarFragment fragment = new solicitarFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_solicitar, container, false);
+    }
+}
