8bc1820d156e02c7a5092aae17023c2352e3d3db
cpmaradei <cpmaradey@gmail.com>
2025-06-10 10:29:38 -0500

    cambios api

--- app/src/main/java/com/example/proyecto/HomeFragment.java
+++ app/src/main/java/com/example/proyecto/HomeFragment.java
@@ -1,67 +1,92 @@
 package com.example.proyecto;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.proyecto.Adaptadores.HeroesAdaptador;
 import com.example.proyecto.ProyectoApp.SuperHeroes;
+import com.example.proyecto.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
-    private RecyclerView recyclerView;
+    Button btn_ver_detalle;
 
+    private RecyclerView rcv_Heroes;
+    private HeroesAdaptador adapter;
     private List<SuperHeroes> superHeroesList;
 
+    private final String publicKey = "be976077e534b19bcd4a2840fa3eba7f";
+    private final String hash = "ec79668fd48c5ad4b02e917abb9839c7";
+    private final String ts = "1";
+    private final String url = "https://gateway.marvel.com/v1/public/characters?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
 
-
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        rcv_Heroes = view.findViewById(R.id.rcv_Heroes);
+        rcv_Heroes.setLayoutManager(new LinearLayoutManager(getContext()));
+
         superHeroesList = new ArrayList<>();
+        adapter = new HeroesAdaptador(superHeroesList);
+        rcv_Heroes.setAdapter(adapter);
+
         cargarSuperHeroes();
+
+        return view;
     }
 
     private void cargarSuperHeroes() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> procesarRespuesta(response),
+                error -> Toast.makeText(getContext(), "Error de conexión", Toast.LENGTH_SHORT).show()
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(requireContext());
+        queue.add(request);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    private void procesarRespuesta(JSONObject response) {
+        try {
+            JSONArray results = response.getJSONObject("data").getJSONArray("results");
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject personaje = results.getJSONObject(i);
+                String nombre = personaje.getString("name");
+                String descripcion = personaje.getString("description");
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+                JSONObject thumbnail = personaje.getJSONObject("thumbnail");
+                String imagenPath = thumbnail.getString("path");
+                String extension = thumbnail.getString("extension");
+                String imagenUrl = imagenPath + "." + extension;
 
+                superHeroesList.add(new SuperHeroes(imagenUrl, nombre, descripcion));
+            }
 
+            adapter.notifyDataSetChanged();
+
+        } catch (JSONException e) {
+            Toast.makeText(getContext(), "Error en el servidor, intente más tarde", Toast.LENGTH_SHORT).show();
+        }
     }
 }
